1bade33218332d09478742d4ba6c41ce22d10229
Kalin <kalindobrinov@abv.bg>
2023-11-14 19:24:10 +0200

    Setup database in startup script

--- start_servers.bat
+++ start_servers.bat
@@ -1,5 +1,19 @@
 @echo off
+
 cd ExpenseTrackerBackend
+
+SET PGPASSWORD=postgres
+psql -U postgres -c "\q"
+if %ERRORLEVEL% neq 0 (
+    echo PostgreSQL is not installed or not configured correctly.
+    exit /b
+)
+
+psql -U postgres -c "SELECT 1 FROM pg_database WHERE datname = 'expenseitdb'" | find "1" > nul
+if %ERRORLEVEL% neq 0 (
+    psql -U postgres -c "CREATE DATABASE expenseitdb"
+)
+
 start cmd /k "mvn spring-boot:run"
 cd ..
 
@@ -8,7 +22,7 @@ start cmd /k "npm install && ng serve"
 cd ../..
 
 cd ExpenseTrackerUtilServer
-start cmd /k "pip install -r requirements.txt && py ./main.py"
+start cmd /k "pip install -r requirements.txt && python ./main.py"
 cd ..
 
 echo All servers are starting...
